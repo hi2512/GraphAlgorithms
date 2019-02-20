53fcb36507fb0ce3cc519d758c35eee3fa5acc3a
Eric <hiken2512@gmail.com>
2019-02-20 14:50:12 -0600

    added labels

--- hw2.cpp
+++ hw2.cpp
@@ -14,21 +14,31 @@ struct PageNode {
 	int label;
 	double curVal;
 	double prevVal;
+	vector<tuple<PageNode*, int>>*  connections;
 };
 
 void doPageRank(vector<PageNode> pn) {
 
 }
-
+/*
 vector<PageNode>* createPageNodes(std::vector<int> & rp, std::vector<int> & ci, std::vector<int> & ai) {
 	vector<PageNode> * res = new vector<PageNode>();
 	for(int i = 1; i <= rp.size(); i++) {
-		res->push_back(PageNode{i, 1.0 / rp.size(), -1.0});
+		res->push_back(PageNode{i, 1.0 / rp.size(), -1.0, new vector<PageNode*, int>()});
 	}
+
+	int rIndex = 0;
+	for(int i = 0; i < ci.size(); i++) {
+		if((rIndex + 1 < rp.size()) && (rp.at(rIndex + 1) - 1 == i)) {
+			rIndex++;
+		}
+		res->at(rIndex - 1).connections->push_back(res->at());
+	}
+
 	return res;
 }
-
-void dimacsToCSR(ifstream & dimacfile, std::vector<int> & rp, std::vector<int> & ci, std::vector<int> & ai) {
+*/
+void dimacsToCSR(ifstream & dimacfile, std::vector<int> & rp, std::vector<int> & ci, std::vector<int> & ai, std::vector<int> & nodeLabels) {
 	std::vector<tuple<int, int, int>> res;
 	string line;
 	char fline[50];
@@ -46,6 +56,9 @@ void dimacsToCSR(ifstream & dimacfile, std::vector<int> & rp, std::vector<int> &
 	int curNode = 0;
 	for(auto t : res) {
 		cout << get<0>(t) << " " << get<1>(t) << " " << get<2>(t) << endl;
+		if(!count(nodeLabels.begin(), nodeLabels.end(), get<0>(t))) {
+			nodeLabels.push_back(get<0>(t));
+		}
 		if(curNode != get<0>(t)) {
 			rp.push_back(ci.size() + 1);
 		}
@@ -55,7 +68,7 @@ void dimacsToCSR(ifstream & dimacfile, std::vector<int> & rp, std::vector<int> &
 	}
 }
 
-void csrToDimacs(ofstream & outfile, std::vector<int> & rp, std::vector<int> & ci, std::vector<int> & ai) {
+void csrToDimacs(ofstream & outfile, std::vector<int> & rp, std::vector<int> & ci, std::vector<int> & ai, std::vector<int> & nodeLabels) {
 	outfile << "p sp " << rp.size() << " " << ci.size() << endl;
 	int rIndex = 0;
 	for(int i = 0; i < ci.size(); i++) {
@@ -63,7 +76,7 @@ void csrToDimacs(ofstream & outfile, std::vector<int> & rp, std::vector<int> & c
 		if((rIndex + 1 < rp.size()) && (rp.at(rIndex + 1) - 1 == i)) {
 			rIndex++;
 		}
-		outfile << rIndex + 1 << " ";
+		outfile << nodeLabels.at(rIndex) << " ";
 		outfile << ci.at(i) << " " << ai.at(i) << endl;
 	}
 }
@@ -92,7 +105,8 @@ int main(int argc, char * argv[], char * env[]) {
 	vector<int> rp;
 	vector<int> ci;
 	vector<int> ai;
-	dimacsToCSR(dimacfile, rp, ci, ai);
+	vector<int> nodeLabels;
+	dimacsToCSR(dimacfile, rp, ci, ai, nodeLabels);
 	for(auto i : rp) {
 		cout << i << " ";
 	}
@@ -105,10 +119,14 @@ int main(int argc, char * argv[], char * env[]) {
 		cout << i << " ";
 	}
 	cout << endl;
+	for(auto i : nodeLabels) {
+		cout << i << " ";
+	}
+	cout << endl;
 
 	ofstream outfile;
 	outfile.open("1.dimacs");
-	csrToDimacs(outfile, rp, ci, ai);
+	csrToDimacs(outfile, rp, ci, ai, nodeLabels);
 	outfile.close();
 
 	return 0;
