f059790092135c019c05252ce30ec5b6fda5fe90
Ramsey Hashem <rhashem@cs.utexas.edu>
2019-02-28 16:10:40 -0600

    Should be final commit

--- README.txt
+++ README.txt
@@ -1,9 +1,31 @@
-Eric Nguyen Ramsey Hashem
-Use make to create hw2 program.
-hw2 takes in one argument, the input dimacs file.
-The outputs are the dimacs file (additional edges
+CS 377P - Assignment 2: Graph Algorithms
+----------------------------------------
+
+Eric Nguyen 
+Ramsey Hashem
+
+hw2 takes in one argument, the input DIMACS file.
+The outputs are the DIMACS file (additional edges
 between the same nodes are removed with only the
 largest weight remaining following the homework
 spec), the node number and label file, and a file
-containing the histogram. The pagerank is output
-to standard output.
+containing the histogram. 
+
+To compile and run:
+- Navigate to the GraphAlgorithms directory
+- Use 'make' to compile
+- To run, do ./hw2 <filename>
+- For example, to run with the wiki.dimacs file, do:
+  ./hw2 wiki.dimacs
+
+The PageRank is output to standard output. 
+The results of the histogram can be found in histogram.txt
+The conversion from CSR to DIMACS is output to out.dimacs
+
+The raw PageRank results can be viewed in:
+wikipr.txt
+road-NYpr.txt
+rmat15pr.txt
+
+
+
--- hw2.cpp
+++ hw2.cpp
@@ -10,7 +10,6 @@
 
 using namespace std;
 
-
 struct PageNode {
 	int number;
 	int label;
@@ -24,13 +23,12 @@ void scaleValues(vector<PageNode> * pn) {
 	for(PageNode n : *pn) {
 		sum += n.curVal;
 	}
-	//cout << "Sum is " << sum << endl;
 	for(PageNode& n : *pn) {
 		n.curVal /= sum;
 	}
 }
 
-//checks to see if change below the threshold has occured
+// Checks to see if change below the threshold has occured
 bool terminateThreshold(vector<PageNode>* pn, double threshold) {
 	for(auto it = pn->begin(); it != pn->end(); ++it) {
 		PageNode cur = *it;
@@ -58,12 +56,13 @@ void printPageVals(vector<PageNode>* pn) {
 	cout << "SUM: " << sum << endl;
 }
 
+// Calculates page rank values on a set of data
 vector<PageNode>* doPageRank(std::vector<int> & rp, std::vector<int> & ci, std::vector<int> & ai, std::vector<int> nodeLabels, int nodeNum, double damp, double threshold) {
 	vector<PageNode>* pns = new vector<PageNode>();
 	for(int i = 1; i <= nodeNum; i++) {
 		pns->push_back(PageNode{i, i, 0, 1.f / nodeNum, 1.f});
 	}
-	//set outlinks
+	// Set outlinks
 	for(int i = 0; i < rp.size(); i++) {
 		if(i == (rp.size() - 1)) {
 			pns->at(nodeLabels.at(i) - 1).outLinks = ci.size() + 1 - rp.at(i);
@@ -72,16 +71,16 @@ vector<PageNode>* doPageRank(std::vector<int> & rp, std::vector<int> & ci, std::
 		}
 	}
 	do {
-		//set previous value to value of last iteration
+		// Set previous value to value of last iteration
 		for(PageNode& n : *pns) {
 			n.prevVal = n.curVal;
 			n.curVal = 0;
 		}
-		//gather sum
+		// Gather sum
 		for(int i = 0; i < rp.size(); i++) {
 			int ciIndex = rp.at(i) - 1;
 			int linkNum = pns->at(nodeLabels.at(i) - 1).outLinks;
-			//run through all links
+			// Run through all links
 			for(int j = 0; j < linkNum; j++) {
 				PageNode& fromNode = pns->at(nodeLabels.at(i) - 1);
 				PageNode& toNode =  pns->at(ci.at(ciIndex + j) - 1);
@@ -106,15 +105,16 @@ bool tupleOrder(tuple<int, int, int> x, tuple<int, int, int> y) {
 	} else {
 		return get<0>(x) < get<0>(y);
 	}
-
 }
 
+// Converts from DIMACS format to CSR format
 void dimacsToCSR(ifstream & dimacfile, std::vector<int> & rp, std::vector<int> & ci, std::vector<int> & ai, std::vector<int> & nodeLabels,
 				std::vector<tuple<int, int, int>> & res) {
 	string line;
 	char fline[50];
 	int nodeFrom, nodeTo, weight;
-	//read in from file
+
+	// Read in from file
 	while(getline(dimacfile, line)) {
 		strcpy(fline, line.c_str());
 		strtok(fline, " ");
@@ -123,13 +123,13 @@ void dimacsToCSR(ifstream & dimacfile, std::vector<int> & rp, std::vector<int> &
 		weight = atoi(strtok(NULL, " "));
 		res.push_back(tuple<int, int, int>(nodeFrom, nodeTo, weight));
 	}
-	//arent they already sorted though?
+
 	sort(res.begin(), res.end(), tupleOrder);
-	//keeps only highest weight duplicates
+
+	// Keeps only highest weight duplicates
 	res.erase(unique(res.begin(), res.end(), [](tuple<int, int, int> x, tuple<int, int, int> y) {return (get<0>(x) == get<0>(y)) && (get<1>(x) == get<1>(y));}), res.end());
 	int curNode = 0;
 	for(auto t : res) {
-		//cout << get<0>(t) << " " << get<1>(t) << " " << get<2>(t) << endl;
 		if(!count(nodeLabels.begin(), nodeLabels.end(), get<0>(t))) {
 			nodeLabels.push_back(get<0>(t));
 		}
@@ -142,6 +142,7 @@ void dimacsToCSR(ifstream & dimacfile, std::vector<int> & rp, std::vector<int> &
 	}
 }
 
+// Converts from CSR format to DIMACS format
 void csrToDimacs(ofstream & outfile, std::vector<int> & rp, std::vector<int> & ci, std::vector<int> & ai, std::vector<int> & nodeLabels, int nodeNum) {
 	outfile << "p sp " << nodeNum << " " << ci.size() << endl;
 	int rIndex = 0;
@@ -155,12 +156,14 @@ void csrToDimacs(ofstream & outfile, std::vector<int> & rp, std::vector<int> & c
 	}
 }
 
+// Outputs the histogram to a file
 void output_histogram(ofstream& outfile, std::vector<int> & rp, std::vector<int> & ci, std::vector<int> & ai, std::vector<int> nodeLabels, int nodeNum) {
 	vector<PageNode>* pns = new vector<PageNode>();
 	for(int i = 1; i <= nodeNum; i++) {
 		pns->push_back(PageNode{i, i, 0, 1.f / nodeNum, 1.f});
 	}
-	//set outlinks
+	// Analyze the outgoing edges from each node
+	// Keep trakc of the max number of outgoing edges
 	int maxLinks = -1;
 	for(int i = 0; i < rp.size(); i++) {
 		if(i == (rp.size() - 1)) {
@@ -177,10 +180,14 @@ void output_histogram(ofstream& outfile, std::vector<int> & rp, std::vector<int>
 			}
 		}
 	}
+
+	// Determine the frequency of nodes for a given number of outgoing edges
 	vector<int> links(maxLinks + 1, 0);
 	for(int j = 0; j < pns->size(); ++j) {
 		++links[(*pns)[j].outLinks];
 	}
+
+	// Output to file
 	for(int k = 0; k < links.size(); ++k) {
 		outfile << k << " " << links[k] << endl;
 	}
@@ -198,57 +205,40 @@ int main(int argc, char * argv[], char * env[]) {
 	strcpy(fline, firstline.c_str());
 
 	char * tok;
-	//p
+	// p
 	strtok(fline, " ");
-	//sp
+	// sp
 	strtok(NULL, " ");
 	tok = strtok(NULL, " ");
 	int nodes = atoi(tok);
 	tok = strtok(NULL, " ");
 	int edges = atoi(tok);
 
+	// COO format data
 	vector<int>* rp = new vector<int>();
 	vector<int>* ci = new vector<int>();
 	vector<int>* ai = new vector<int>();
 	vector<int>* nodeLabels = new vector<int>();
 	vector<tuple<int, int, int>>* res = new vector<tuple<int, int, int>>();
+
+	// Converting to CSR
 	dimacsToCSR(dimacfile, *rp, *ci, *ai, *nodeLabels, *res);
 	ofstream lfile;
 	lfile.open("labels.txt");
-	/*
-	for(auto i : *rp) {
-		lfile << i << " ";
-	}
-	lfile << endl;
-	for(auto i : *ci) {
-		lfile << i << " ";
-	}
-	lfile << endl;
-	for(auto i : *ai) {
-		lfile << i << " ";
-	}
-	lfile << endl;
-	for(auto i : *nodeLabels) {
-		lfile << i << " ";
-	}
-	lfile << endl;
-	for(auto i : *res) {
-		lfile << get<0>(i) << " " << get<1>(i) << " " << get<2>(i) << endl;
-	}
-	lfile << endl;
-	*/
 	for(int i = 0; i < nodeLabels->size(); i++) {
 		lfile << i + 1 << " " << nodeLabels->at(i) << endl;
 	}
 	lfile.close();
 
-	//cout << "Histogram:\n";
+	// Histogram
+	cout << "Creating Histogram:\n";
 	ofstream histogram_outfile;
 	histogram_outfile.open("histogram.txt");
 	output_histogram(histogram_outfile, *rp, *ci, *ai, *nodeLabels, nodes);
 	histogram_outfile.close();
-	//cout << "Done Histogram\n";
+	cout << "Done Creating Histogram\n";
 
+	// Output to dimacs
 	ofstream outfile;
 	outfile.open("out.dimacs");
 	csrToDimacs(outfile, *rp, *ci, *ai, *nodeLabels, nodes);
